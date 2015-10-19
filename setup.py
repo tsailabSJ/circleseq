c2b82e192718a6ab60d1c38e91c6c5ead4a1997d
Ved Topkar <vedtopkar@gmail.com>
2015-10-19 17:25:53 -0400

    Scaffolded out project

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='circleseq',
+    version='0.1.0',
+    description="An easy to use bioinformatic pipeline for the CIRCLE-seq assay.",
+    author="Shengdar Q Tsai, Martin Aryee, Ved V Topkar",
+    author_email='STSAI4@mgh.harvard.edu, Aryee.Martin@mgh.harvard.edu, vedtopkar@gmail.com',
+    url='https://github.com/aryeelab/circleseq',
+    packages=[
+        'circleseq',
+    ],
+    package_dir={'circleseq':
+                 'circleseq'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="AGPL",
+    keywords='circleseq',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: Unix',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
