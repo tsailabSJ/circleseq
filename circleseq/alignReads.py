552f06151f2c512d03d5308bbeea44dcaae55a76
stsai <shengdar@gmail.com>
2017-10-30 21:31:31 -0500

    Update samtools command

--- circleseq/alignReads.py
+++ circleseq/alignReads.py
@@ -46,9 +46,9 @@ def alignReads(BWA_path, HG19_path, read1, read2, outfile):
     # Run paired end alignment against the genome
     logger.info('Running paired end mapping for {0}'.format(sample_name))
     bwa_alignment_command = '{0} mem {1} {2} {3} > {4}'.format(BWA_path, HG19_path, read1, read2, sam_filename)
-    samtools_sam_to_bam_command = 'samtools sort {1}    -f {0}'.format(bam_filename, sam_filename)
+    samtools_sam_to_bam_command = 'samtools sort {1} -f {0}'.format(bam_filename, sam_filename)
     samtools_index_command = 'samtools index {0}'.format(bam_filename)
-    samtools_sort_by_name_command = 'samtools sort -o {0} -n {1}'.format("".join([base_name, '_sorted.bam']), bam_filename)
+    #samtools_sort_by_name_command = 'samtools sort -o {0} -n {1}'.format("".join([base_name, '_sorted.bam']), bam_filename)
 
     # Open the outfile and redirect the output of the alignment to it.
     logger.info(bwa_alignment_command)
@@ -66,9 +66,9 @@ def alignReads(BWA_path, HG19_path, read1, read2, outfile):
     logger.info('Indexing for {0} complete.'.format(sample_name))
 
     # Sort SAM file by name
-    logger.info(samtools_sort_by_name_command)
-    subprocess.check_call(samtools_sort_by_name_command, shell=True)
-    logger.info('Sorting for {0} by name complete.'.format(sample_name))
+    #logger.info(samtools_sort_by_name_command)
+    #subprocess.check_call(samtools_sort_by_name_command, shell=True)
+    #logger.info('Sorting for {0} by name complete.'.format(sample_name))
 
 
 
